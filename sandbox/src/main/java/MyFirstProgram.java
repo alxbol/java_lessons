bfaa8e2dba4b8014d7d08aa8c57e88ab32884704
Alexey <bolshakov.alexey@gmail.com>
2021-07-01 21:23:00 +0400

    Fixed MyFirstProgram

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -1,5 +1,7 @@
-class MyFirstProgram{  
-	public static void main(String args[]){  
-		System.out.println("Hello, world!);  
-	}  
-}  
+package src.main.java;
+
+class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
