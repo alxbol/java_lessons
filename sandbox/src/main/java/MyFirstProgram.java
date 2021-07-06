a0bbdb0fb30640473ccf9e6fca7d01230d43d87a
Alexey <bolshakov.alexey@gmail.com>
2021-07-06 20:06:54 +0400

    Задание №2

--- sandbox/src/main/java/MyFirstProgram.java
+++ sandbox/src/main/java/MyFirstProgram.java
@@ -2,6 +2,12 @@ package src.main.java;
 
 class MyFirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Point p1 = new Point();
+        p1.x = 300;
+        p1.y = 300;
+        Point p2 = new Point();
+        p2.x = 400;
+        p2.y = 400;
+        System.out.println(Point.distance(p1, p2));
     }
 }
