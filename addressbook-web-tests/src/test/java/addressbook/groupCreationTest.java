7dde7927dded0ff4efe279154901428340b1b8f1
Alexey <bolshakov.alexey@gmail.com>
2021-07-14 00:51:05 +0400

    Добавлены вспомогательные методы

--- addressbook-web-tests/src/test/java/addressbook/groupCreationTest.java
+++ addressbook-web-tests/src/test/java/addressbook/groupCreationTest.java
@@ -20,26 +20,50 @@ public class groupCreationTest {
     driver = new ChromeDriver();
     new WebDriverWait(driver, Duration.ofSeconds(30));
     driver.get("http://localhost/addressbook/index.php");
+    login("admin", "secret");
+  }
+
+  private void login(String user, String password) {
     driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("user")).sendKeys(user);
     driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.name("pass")).sendKeys(password);
     driver.findElement(By.xpath("//input[@value='Login']")).click();
   }
 
   @Test
   public void testUntitledTestCase() {
-    driver.findElement(By.linkText("groups")).click();
-    driver.findElement(By.name("new")).click();
-    driver.findElement(By.name("group_name")).clear();
-    driver.findElement(By.name("group_name")).sendKeys("test1");
-    driver.findElement(By.name("group_header")).clear();
-    driver.findElement(By.name("group_header")).sendKeys("test2");
-    driver.findElement(By.name("group_footer")).clear();
-    driver.findElement(By.name("group_footer")).sendKeys("test3");
-    driver.findElement(By.name("submit")).click();
-    driver.findElement(By.linkText("group page")).click();
+    goToGroupPage();
+    initGroupCreation();
+    fillGroupForm("test1", "test2", "test3");
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+  private void returnToGroupPage() {
     driver.findElement(By.linkText("Logout")).click();
+  }
+
+  private void submitGroupCreation() {
+    driver.findElement(By.linkText("group page")).click();
+  }
+
+  private void fillGroupForm(String name, String header, String footer) {
+    driver.findElement(By.name("group_name")).clear();
+    driver.findElement(By.name("group_name")).sendKeys(name);
+    driver.findElement(By.name("group_header")).clear();
+    driver.findElement(By.name("group_header")).sendKeys(header);
+    driver.findElement(By.name("group_footer")).clear();
+    driver.findElement(By.name("group_footer")).sendKeys(footer);
+    driver.findElement(By.name("submit")).click();
+  }
+
+  private void initGroupCreation() {
+    driver.findElement(By.name("new")).click();
+  }
+
+  private void goToGroupPage() {
+    driver.findElement(By.linkText("groups")).click();
   }
 
   @AfterClass(alwaysRun = true)
