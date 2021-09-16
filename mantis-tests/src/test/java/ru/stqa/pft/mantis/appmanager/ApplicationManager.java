188f92404db54b348d0e45d24229e50531b4d722
Alexey <bolshakov.alexey@gmail.com>
2021-09-16 17:30:40 +0400

    Лекция 8.3. Прямое взаимодействие с сервером по протоколу HTTP

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -1,14 +1,9 @@
 package ru.stqa.pft.mantis.appmanager;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.remote.BrowserType;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.Duration;
 import java.util.Properties;
 
 public class ApplicationManager {
@@ -24,17 +19,27 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
-        if (browser.equals(BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(BrowserType.FIREFOX)) {
-            wd = new FirefoxDriver();
-        }
-
-        new WebDriverWait(wd, Duration.ofSeconds(30));
-        wd.get(properties.getProperty("web.baseUrl"));
+//        if (browser.equals(BrowserType.CHROME)) {
+//            wd = new ChromeDriver();
+//        } else if (browser.equals(BrowserType.FIREFOX)) {
+//            wd = new FirefoxDriver();
+//        }
+//
+//        new WebDriverWait(wd, Duration.ofSeconds(30));
+//        wd.get(properties.getProperty("web.baseUrl"));
+        String prop = properties.getProperty("web.baseUrl");
     }
+
 
     public void stop() {
         wd.quit();
+    }
+
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
     }
 }
